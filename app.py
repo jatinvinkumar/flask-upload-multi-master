249447ba9a7cccff9d5b3c2607d84506061959db
jatin <jatinvinkumar@gmail.com>
2022-06-04 12:36:42 -0500

    file pathing for constants.json

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def allowed_file(filename):
 
 #define and initiate the JSON const object 
 # Opening JSON file
-f = open('constants.json')
+f = open('./constants.json')
   
 # returns JSON object as 
 # a dictionary
